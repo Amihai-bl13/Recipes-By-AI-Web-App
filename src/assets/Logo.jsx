dff15b1c6071699e0e0458d71c01953b3226c588
Amihai Blekherov <amihaibl@post.bgu.ac.il>
2025-06-16 12:48:35 +0300

    Fixed an issue with the logo - was interfering with the header control buttons

--- src/assets/Logo.jsx
+++ src/assets/Logo.jsx
@@ -8,6 +8,7 @@ export default function Logo() {
         width: '100%',
         height: '100%',
         pointerEvents: 'none', // don't block clicks
+        zIndex: -1, // ensure it stays behind other elements
       }}
     >
       <svg
